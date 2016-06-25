a31cfef750146f6bbbcbe846ac21109ea2fb5b77
elnw <xxelnwxx@gmail.com>
2016-06-25 18:24:38 -0500

    Nueva implementacion de la clase CItem

--- Item.cpp
+++ Item.cpp
@@ -5,75 +5,19 @@ CItem::CItem()
 {
 	aumAtaque = 0;
 	aumDefensa = 0;
-	nombre = new char[50];
+
 }
 
 
 CItem::~CItem()
 {
-	delete nombre;
+	
 }
 
-void CItem::setEstadistica(bool recibir){
-	if (recibir == true){
-		std::ifstream archivo("items.txt");
-		char *cadena = new char[100];
-		char *cad2 = new char[2];
-		char *cad3 = new char[2];
-		srand(3);
-		int probabilidad = rand() % 151;
-		int lineas;
-		if (probabilidad > 140){
-			lineas = rand() % 2 + 5;
-		}
-		else
-			if (probabilidad < 140 && probabilidad>100){
-				lineas = rand() % 2 + 3;
-			}
-			else{
-				lineas = rand() % 2 + 1;
-			}
-
-
-			while (lineas<0){
-				archivo.getline(cadena, 100);
-				lineas--;
-			}
-			int contador = 0;
-			int aux = 0;
-			int pos = 0;
-
-			while (contador == 0){
-				nombre[pos] = cadena[pos];
-				pos++;
-				if (cadena[pos + 1] == ','){
-					contador++;
-				}
-			}
-			pos += 2;
-			while (contador == 1){
-				cad2[aux] = cadena[pos];
-				pos++;
-				aux++;
-				if (cadena[pos + 1] == ','){
-					contador++;
-				}
-			}
-			pos += 2;
-			aux = 0;
-			while (contador == 2){
-				cad3[aux] = cadena[pos];
-				pos++;
-				aux++;
-				if (cadena[pos + 1] == ','){
-					contador++;
-				}
-			}
-
-			aumAtaque = (int)cad2;
-			aumDefensa = (int)cad3;
-
-	}
+void CItem::setEstadistica(std::string nombre, int aumDefensa, int aumAtaque){
+	this->nombre = nombre;
+	this->aumAtaque = aumAtaque;
+	this->aumDefensa = aumDefensa;
 	
 
 }
